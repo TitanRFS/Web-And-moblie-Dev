9fdfc4626e0d338673c0921a5e73eb80d8029b15
Christos Gianniotis <115632527+TitanRFS@users.noreply.github.com>
2022-12-13 18:27:07 +0200

    Create form_post.php

---
+++ form_post.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="welcome_post.php" method="POST">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+	Password:<input type="password" id="pwd" name="pwd"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
