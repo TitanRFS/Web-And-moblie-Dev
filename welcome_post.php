4de744d7de219e7647b12d83feea51faab338086
Christos Gianniotis <115632527+TitanRFS@users.noreply.github.com>
2022-12-13 18:28:07 +0200

    Create welcome_post.php

---
+++ welcome_post.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password=$_POST['pwd'];
+
+echo 'Your name is : ' . $name . '<br>';
+echo 'Your email is: ' . $email . '<br>';
+echo 'Your Password is'.$password.'<br>';
+
+?>
